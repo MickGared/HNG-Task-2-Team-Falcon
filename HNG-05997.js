aaa0c2125b9fde39b15c356af4c142265235c6fc
MickB <66332295+MickGared@users.noreply.github.com>
2020-06-05 00:41:23 +0100

    HNG-05997.js
    Resubmission

---
+++ HNG-05997.js
@@ -0,0 +1,9 @@
+var text1 = "Hello World, this is ";
+var fullName = "Balogun Ayobami ";
+var text2 = "with HNGi7 ID ";
+var id = "HNG-05997 ";
+var text3 = "using ";
+var language = "Javascript ";
+var text4 = "for stage 2 task. ";
+var email = "mickgared9@gmail.com";
+console.log(text1 + fullName + text2 + id + text3 + language + text4 + email);
